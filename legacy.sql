011cedc2f4cb85af19eb039350ff265d36e43f97
alfonssbe <alfons@sbacoustics.com>
2026-03-04 07:29:35 +0700

    updated to the newest version

--- legacy.sql
+++ legacy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2026 at 07:12 AM
+-- Generation Time: Mar 04, 2026 at 01:15 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
